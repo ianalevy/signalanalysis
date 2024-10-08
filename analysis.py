35eb7e3c1a682b773f0f94e27a644de94f1ac873
Ian Alevy <ian.alevy@gmail.com>
2024-10-07 18:54:21 -0600

    plot hist

--- analysis.py
+++ analysis.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Callable
 
 import numpy as np
+import pyqtgraph as pg
 
 
 @dataclass
@@ -15,11 +17,40 @@ class HistogramResults:
         if self.centers is None:
             self.centers = (self.bins[1:] + self.bins[:-1]) / 2
 
+    def interp_distr(self) -> Callable:
+        return np.interp(self.centers, self.counts)
+
 
 def compute_histogram(
     data: np.ndarray,
-    bins: int | None = None,
+    bins: int = 10,
 ) -> HistogramResults:
     counts, edges = np.histogram(data, bins=bins)
 
     return HistogramResults(edges, counts)
+
+
+def plot_hist(win, hist: HistogramResults):
+    win.resize(800, 480)
+    win.setWindowTitle("Histogram")
+    plt = win.addPlot()
+
+    bgi = pg.BarGraphItem(
+        x0=hist.bins[:-1],
+        x1=hist.bins[1:],
+        height=hist.counts,
+        pen="w",
+        brush=(0, 0, 255, 150),
+    )
+    plt.addItem(bgi)
+
+
+if __name__ == "__main__":
+    rng = np.random.default_rng(seed=42)
+    data = rng.normal(loc=3, size=1000)
+    hist = compute_histogram(data, bins=50)
+
+    win = pg.GraphicsLayoutWidget(show=True)
+    plot_hist(win, hist)
+
+    pg.exec()
