c0e3a86c4d72a432a1436eddfc0e93036b270df2
Ian Alevy <ian.alevy@gmail.com>
2024-10-28 19:15:44 -0600

    test times match

--- test/test_pulse_simulator.py
+++ test/test_pulse_simulator.py
@@ -1,9 +1,9 @@
 import unittest
 
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
-from pulse_simulator import frame_array
+from pulse_simulator import Pulse, frame_array, make_signal
 
 
 class TestAnalysis(unittest.TestCase):
@@ -28,6 +28,15 @@ class TestAnalysis(unittest.TestCase):
         res = pulse.sample_pulse(1e-2)
 
         assert len(res) == 20 / (1.0e-2)
+
+    def test_make_signal(self):
+        (times, signal) = make_signal(0.1, 0.01, 3, 0.02)
+        assert len(times) == 32
+        stop = 3 * 0.1 + 0.02
+        assert_array_almost_equal(
+            times,
+            np.linspace(0, stop, num=int(stop / 0.01)),
+        )
 
 
 if __name__ == "__main__":
