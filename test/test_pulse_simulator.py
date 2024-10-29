feac2eb83fedcfb14fed82cbbf64b6b1d891b080
Ian Alevy <ian.alevy@gmail.com>
2024-10-28 18:27:58 -0600

    first good unit test

--- test/test_pulse_simulator.py
+++ test/test_pulse_simulator.py
@@ -1,9 +1,21 @@
 import unittest
 
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from pulse_simulator import frame_array
+
 
 class TestAnalysis(unittest.TestCase):
-    def test_1(self):
-        assert 4 == 4
+    def test_frame_array(self):
+        data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+        res = frame_array(data, 2)
+        assert_array_equal(data, np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+
+        assert_array_equal(
+            np.stack(res),
+            np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 0]]),
+        )
 
 
 if __name__ == "__main__":
