2ff81ffd2c54174d956679c0b1318d8ca1c946f1
Ian Alevy <ian.alevy@gmail.com>
2024-10-31 06:07:22 -0600

    test detector

--- test/test_pulse_simulator.py
+++ test/test_pulse_simulator.py
@@ -3,7 +3,14 @@ import unittest
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_array_equal
 
-from pulse_simulator import Pulse, calc_norm, frame_array, make_signal
+from pulse_simulator import (
+    Pulse,
+    calc_norm,
+    detector,
+    frame_array,
+    make_signal,
+    try_pris,
+)
 
 
 class TestAnalysis(unittest.TestCase):
@@ -44,6 +51,29 @@ class TestAnalysis(unittest.TestCase):
 
         assert np.abs(res - 5.0 / 3) < 0.0001
 
+    def test_detector(self):
+        pw_s = 0.03
+        sample_rate_s = 0.01
+        (times, data) = make_signal(0.1, sample_rate_s, 3, pw_s)
+
+        # print(data)
+        res = detector(data, sample_rate_s, pw_s)
+        # print(res)
+        # assert 4 == 5
+        # fmt: off
+        true_detects = np.array(
+            [
+                0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0,
+            ],
+        )
+
+        assert_array_equal(
+            res,
+            true_detects,
+        )
 
 if __name__ == "__main__":
     unittest.main()
