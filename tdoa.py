9844dfc98bdc1fd683970e3bff6090c35523862f
Ian Alevy <ian.alevy@gmail.com>
2024-11-27 05:28:16 -0700

    start of tdoa code

---
+++ tdoa.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import logging
+
+logging.getLogger("__name__")
+
+
+def fun_1():
+    return 5
+
+
+if __name__ == "__main__":
+    print("hi")
---
+++ test/test_tdoa.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestAnalysis(unittest.TestCase):
+    def test_1(self):
+        assert 4 == 5
+
+
+if __name__ == "__main__":
+    unittest.main()
