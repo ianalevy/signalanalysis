df3e18aa4e174e6773ed5e3bf8d989bbf525affa
Ian Alevy <ian.alevy@gmail.com>
2024-11-29 06:48:48 -0700

    more tests

--- test/test_pulse_simulator.py
+++ test/test_pulse_simulator.py
@@ -45,11 +45,6 @@ class TestPdw(unittest.TestCase):
             ),
         )
     def test_moving_average(self):
-        data = np.array([1, 1, 1, 5, 1, 1, 1, 1])
-        res = moving_average(data, 3)
-        truth = np.array([2 / 3, 1, 7 / 3, 7 / 3, 7 / 3, 1, 1, 2 / 3])
-        assert_array_almost_equal(res, truth)
-
         data = np.array([1, 1, 1, 2.2, 5.5, 1, 1, 1, 1])
         # order 1 does nothing
         res = moving_average(data, 1)
@@ -64,6 +59,18 @@ class TestPdw(unittest.TestCase):
         data = np.array([1, 1, 1, 2.2, 5.5, 1, 1, 1, 1])
         res = moving_average(data, 3)
         truth = np.array([2 / 3, 1, 4.2 / 3, 8.7 / 3, 8.7 / 3, 7.5 / 3, 1, 1, 2 / 3])
+        assert_array_almost_equal(res, truth)
+
+        # window of size 8
+        data = np.array([1, 1, 1, 2.2, 5.5, 1, 1, 1])
+        # order 1 does nothing
+        res = moving_average(data, 1)
+        truth = np.array([1, 1, 1, 2.2, 5.5, 1, 1, 1])
+        assert_array_almost_equal(res, truth)
+
+        data = np.array([1, 1, 1, 5, 1, 1, 1, 1])
+        res = moving_average(data, 3)
+        truth = np.array([0, 1, 7 / 3, 7 / 3, 7 / 3, 1, 1, 2 / 3])
         assert_array_almost_equal(res, truth)
 
 
