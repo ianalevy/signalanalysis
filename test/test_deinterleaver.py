04e9e57b1cabb147b634b7de4b26ce9d729252f9
Ian Alevy <ian.alevy@gmail.com>
2024-12-07 13:16:07 -0700

    some reordering of code

--- test/test_deinterleaver.py
+++ test/test_deinterleaver.py
@@ -7,6 +7,13 @@ from deinterleaver import indices_matching, indices_to_pulse_pairs
 
 
 class TestDeinterleaver(unittest.TestCase):
+    def test_indices_to_pulses(self):
+        in1 = np.array([11, 7, 3, 5, 9, 21])
+        in2 = np.array([2, 6, 8, 4, 16, 12])
+        matches = np.array([[2, 1], [3, 5], [5, 4]])
+        res = indices_to_pulse_pairs(in1, in2, matches)
+        assert_allclose(res, np.array([[3, 6], [5, 12], [21, 16]]))
+
     def test_1(self):
         toa1 = np.array([3.4, 4.5, 5.6, 12.3, 13.4, 14.5, 21.2])
         toa2 = np.array([4.51, 5.2, 5.61, 12.31, 13.41, 14.51])
@@ -20,13 +27,9 @@ class TestDeinterleaver(unittest.TestCase):
             ),
         )
 
-
-    def test_indices_to_pulses(self):
-        in1 = np.array([11, 7, 3, 5, 9, 21])
-        in2 = np.array([2, 6, 8, 4, 16, 12])
-        matches = np.array([[2, 1], [3, 5], [5, 4]])
-        res = indices_to_pulse_pairs(in1, in2, matches)
-        assert_allclose(res, np.array([[3, 6], [5, 12], [21, 16]]))
+        # now case where multiple within tolerance
+        toa1 = np.array([3.4, 4.5, 5.6, 12.3, 13.4, 14.5])
+        toa2 = np.array([4.53, 4.51, 12.31, 13.41, 14.51])
 
 
 if __name__ == "__main__":
