718c04e352f9fbee669e7fa527b90573db240577
Ian Alevy <ian.alevy@gmail.com>
2024-12-08 13:57:26 -0700

    deinterleaver

--- deinterleaver.py
+++ deinterleaver.py
@@ -26,5 +26,29 @@ def indices_to_pulse_pairs(
     return np.column_stack((in1[matches[:, 0]], in2[matches[:, 1]]))
 
 
+def deinterleave_pairs(toa1: np.ndarray, toa2: np.ndarray, tol=0.01) -> np.ndarray:
+    """Deinterleave pairs within tolerance.
+
+    Parameters
+    ----------
+    toa1 : np.ndarray
+    toa2 : np.ndarray
+    tol : float, optional
+        by default 0.01
+
+    Returns
+    -------
+    np.ndarray
+
+    """
+    match_indices = []
+    for in1, time in np.ndenumerate(toa1):
+        indices = np.argwhere(np.abs(toa2 - time) < tol)
+        if len(indices > 0):
+            match_indices.append([in1[0], indices[0][0].tolist()])
+
+    return indices_to_pulse_pairs(toa1, toa2, match_indices)
+
+
 if __name__ == "__main__":
     print("hello")
